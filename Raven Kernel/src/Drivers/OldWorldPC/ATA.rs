08645aa15ae8c4eecba8c7a72ca27d996ff9f4f9
TalonTheRaven <105325988+TalonTheRaven@users.noreply.github.com>
2022-05-30 09:46:44 -0500

    Raven Kernel: Completed ATA Driver

--- Raven Kernel/src/Drivers/OldWorldPC/ATA.rs
+++ Raven Kernel/src/Drivers/OldWorldPC/ATA.rs
@@ -27,9 +27,10 @@ const ATA_ERRORS: [&str; 8] = [
 pub struct ATADrive {
     base: u16,
     is_minor: bool,
-    pub identity_buffer: [u8; 512],
+    pub identity_buffer: [u16; 256],
     pub supports_48lba: bool,
     pub sectors_on_disk: u64,
+    pub is_atapi: bool,
 }
 
 impl ATADrive {
@@ -37,17 +38,59 @@ impl ATADrive {
         Self {
             base,
             is_minor,
-            identity_buffer: [0; 512],
+            identity_buffer: [0; 256],
             supports_48lba: false,
             sectors_on_disk: 0,
+            is_atapi: false,
         }
     }
 
     pub fn Init(&mut self) {
-        todo!();
+        unsafe {
+            u8::write_to_port(self.base+0x6, if self.is_minor {0xB0} else {0xA0});
+            u8::write_to_port(self.base+0x2, 0);
+            u8::write_to_port(self.base+0x3, 0);
+            u8::write_to_port(self.base+0x4, 0);
+            u8::write_to_port(self.base+0x5, 0);
+            u8::write_to_port(self.base+0x7, 0xEC);
+            let status = u8::read_from_port(self.base+0x7);
+            if status == 0 {
+                log::info!("ATA {} {}: No Drive", if self.base == 0x1F0 {"Primary"} else {"Secondary"}, if self.is_minor {"Minor"} else {"Major"});
+                return;
+            }
+            let waitstatus = self.PollWait();
+            if waitstatus.is_err() {
+                log::info!("ATA {} {}: IDENTIFY Failure", if self.base == 0x1F0 {"Primary"} else {"Secondary"}, if self.is_minor {"Minor"} else {"Major"});
+                return;
+            }
+            if u8::read_from_port(self.base+0x4) != 0 && u8::read_from_port(self.base+0x5) != 0 {
+                self.is_atapi = true;
+                log::info!("ATA {} {}: CD/DVD Drive", if self.base == 0x1F0 {"Primary"} else {"Secondary"}, if self.is_minor {"Minor"} else {"Major"});
+                return;
+            }
+            loop {
+                let status = u8::read_from_port(self.base + 0x07);
+                if status & 0x8 != 0 {break};
+                if status & 0x80 != 0 {
+                    log::info!("ATA {} {}: IDENTIFY Failure", if self.base == 0x1F0 {"Primary"} else {"Secondary"}, if self.is_minor {"Minor"} else {"Major"});
+                    return;
+                }
+            }
+            for i in 0..256 {
+                self.identity_buffer[i] = u16::read_from_port(self.base);
+            }
+            if self.identity_buffer[83] & 0x400 == 0x400 {
+                self.supports_48lba = true;
+                self.sectors_on_disk = self.identity_buffer[100] as u64 | ((self.identity_buffer[101] as u64) << 16) | ((self.identity_buffer[102] as u64) << 32) | ((self.identity_buffer[103] as u64) << 48);
+            } else {
+                self.sectors_on_disk = self.identity_buffer[60] as u64 | ((self.identity_buffer[61] as u64) << 16);
+            }
+            log::info!("ATA {} {}: Hard Drive ({} MiB)", if self.base == 0x1F0 {"Primary"} else {"Secondary"}, if self.is_minor {"Minor"} else {"Major"}, self.sectors_on_disk/2048);
+            return;
+        }
     }
 
-    pub fn Delay400ns(&self) {
+    fn Delay400ns(&self) {
         unsafe {
             u8::read_from_port(self.base + 0x07);
             u8::read_from_port(self.base + 0x07);
@@ -56,15 +99,17 @@ impl ATADrive {
         }
     }
 
-    pub fn PollWait(&self) -> Result<(),u8> {
+    fn PollWait(&self) -> Result<(),i16> {
         loop {
             let status = unsafe {u8::read_from_port(self.base + 0x07)};
             if status & 0x80 == 0 {return Ok(())};
-            if status & 0x01 != 0 {return Err(0)};
+            if status & 0x01 != 0 {
+                return Err(0)
+            };
         }
     }
 
-    pub fn Read28(&mut self, lba: u64, count: u16) -> Result<Vec<u8>,u8> {
+    fn Read28(&mut self, lba: u64, count: u16) -> Result<Vec<u8>,i16> {
         unsafe {
             let mut buf = vec![0; count as usize*512];
             u8::write_to_port(self.base+0x6, (if self.is_minor {0xF0} else {0xE0}) | ((lba >> 24) & 0x0F) as u8);
@@ -86,7 +131,7 @@ impl ATADrive {
         }
     }
 
-    pub fn Read48(&mut self, lba: u64, count: u16) -> Result<Vec<u8>,u8> {
+    fn Read48(&mut self, lba: u64, count: u16) -> Result<Vec<u8>,i16> {
         unsafe {
             let mut buf = vec![0; count as usize*512];
             u8::write_to_port(self.base+0x6, if self.is_minor {0x50} else {0x40});
@@ -112,16 +157,82 @@ impl ATADrive {
         }
     }
 
-    pub fn Write28(&mut self, _lba: u64, _buf: &[u8]) -> Result<(),u8> {
-        todo!();
+    fn Write28(&mut self, lba: u64, buf: &[u8]) -> Result<(),i16> {
+        unsafe {
+            let count = buf.len()/512;
+            u8::write_to_port(self.base+0x6, (if self.is_minor {0xF0} else {0xE0}) | ((lba >> 24) & 0x0F) as u8);
+            u8::write_to_port(self.base+0x2, count as u8);
+            u8::write_to_port(self.base+0x3, (lba & 0xFF) as u8);
+            u8::write_to_port(self.base+0x4, ((lba >> 8) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x5, ((lba >> 16) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x7, 0x30);
+            for i in 0..count {
+                self.Delay400ns();
+                self.PollWait()?;
+                for j in 0..256 {
+                    u16::write_to_port(self.base,buf[((i*512)+(j*2)+0) as usize] as u16 | ((buf[((i*512)+(j*2)+1) as usize] as u16) << 8));
+                }
+            }
+            self.Flush();
+            Ok(())
+        }
     }
 
-    pub fn Write48(&mut self, _lba: u64, _buf: &[u8]) -> Result<(),u8> {
-        todo!();
+    fn Write48(&mut self, lba: u64, buf: &[u8]) -> Result<(),i16> {
+        unsafe {
+            let count = buf.len()/512;
+            u8::write_to_port(self.base+0x6, if self.is_minor {0x50} else {0x40});
+            u8::write_to_port(self.base+0x2, ((count & 0xFF00) >> 8) as u8);
+            u8::write_to_port(self.base+0x3, ((lba >> 24) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x4, ((lba >> 32) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x5, ((lba >> 40) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x2, (count & 0xFF) as u8);
+            u8::write_to_port(self.base+0x3, (lba & 0xFF) as u8);
+            u8::write_to_port(self.base+0x4, ((lba >> 8) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x5, ((lba >> 16) & 0xFF) as u8);
+            u8::write_to_port(self.base+0x7, 0x34);
+            for i in 0..count {
+                self.Delay400ns();
+                self.PollWait()?;
+                for j in 0..256 {
+                    u16::write_to_port(self.base,buf[((i*512)+(j*2)+0) as usize] as u16 | ((buf[((i*512)+(j*2)+1) as usize] as u16) << 8));
+                }
+            }
+            self.Flush();
+            Ok(())
+        }
     }
 
-    pub fn Flush(&self) {
-        todo!();
+    fn Flush(&self) {
+        unsafe {
+            u8::write_to_port(self.base+0x7, 0xE7);
+            self.Delay400ns();
+            self.PollWait();
+        }
+    }
+
+    pub fn Read(&mut self, lba: u64, count: usize) -> Result<Vec<u8>,i16> {
+        if lba > 0x0FFFFFFF || count > 0xFF {
+            if self.supports_48lba {
+                return self.Read48(lba,count as u16);
+            } else {
+                return Err(i16::MIN);
+            }
+        } else {
+            return self.Read28(lba,count as u16);
+        }
+    }
+
+    pub fn Write(&mut self, lba: u64, buf: &[u8]) -> Result<(),i16> {
+        if lba > 0x0FFFFFFF || buf.len()/512 > 0xFF {
+            if self.supports_48lba {
+                return self.Write48(lba,buf);
+            } else {
+                return Err(i16::MIN);
+            }
+        } else {
+            return self.Write28(lba,buf);
+        }
     }
 }
 
@@ -130,6 +241,10 @@ pub fn Initalize() {
     for i in lock.iter() {
         if i.class == 0x1 && (i.subclass == 0x1 || i.subclass == 0x5) {
             log::debug!("Computer has ATA, using ATA driver.");
+            PRIMARY_MAJOR.lock().Init();
+            PRIMARY_MINOR.lock().Init();
+            SECONDARY_MAJOR.lock().Init();
+            SECONDARY_MINOR.lock().Init();
             return;
         }
     }
