d2378f02f47ec842d8bcb568ce7623c76dc3764e
TalonTheRaven <105325988+TalonTheRaven@users.noreply.github.com>
2022-06-10 07:25:59 -0500

    Fox Kernel: Added Framebuffer Driver

--- Fox Kernel/src/Drivers/Generic/mod.rs
+++ Fox Kernel/src/Drivers/Generic/mod.rs
@@ -1,9 +1,11 @@
 pub mod UNIXStreamDevs;
 pub mod PseudoTTY;
 pub mod Keyboard;
+pub mod Framebuffer;
 
 pub fn Initalize() {
     UNIXStreamDevs::Initalize();
     PseudoTTY::Initalize();
     Keyboard::Initalize();
+    Framebuffer::Initalize();
 }
