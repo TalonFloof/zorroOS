23a4212f0727e988e591dd4120ba1598450d4f20
TalonFox <105325988+Talon396@users.noreply.github.com>
2022-09-06 17:09:05 -0500

    Oops
    Signed-off-by: TalonFox <105325988+Talon396@users.noreply.github.com>

--- arch/RiscV64/src/arch_api.rs
+++ arch/RiscV64/src/arch_api.rs
@@ -1,7 +1,9 @@
+use interfaces::MachineAPI;
+
 pub struct APIImpl {
 
 }
 
 impl interfaces::ArchAPI for APIImpl {
-    
+    fn GetMachine(&self) -> alloc::boxed::Box<(dyn MachineAPI + 'static)> { todo!() }
 }
--- arch/RiscV64/src/lib.rs
+++ arch/RiscV64/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 use core::arch::global_asm;
 
+extern crate alloc;
+
 pub mod arch_api;
 
 global_asm!(include_str!("asm/bootstrap.S"));
