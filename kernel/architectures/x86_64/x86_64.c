0477f04300644e34683b1fcf9d29cd1964bd34bf
TalonFox <105325988+Talon396@users.noreply.github.com>
2023-01-03 08:32:35 -0600

    Just some tiny changes
    Signed-off-by: TalonFox <105325988+Talon396@users.noreply.github.com>

--- kernel/architectures/x86_64/x86_64.c
+++ kernel/architectures/x86_64/x86_64.c
@@ -1,4 +1,5 @@
 #include <arch/arch.h>
+#include "logger.h"
 
 void x86_64_EarlyInitialize() {}
 
@@ -8,10 +9,12 @@ void x86_64_CLI() { asm volatile("cli"); }
 void x86_64_STI() { asm volatile("sti"); }
 
 IOwlArch owlArch = {
-    .signature = 0x686372416c774f49,
-    .initialize_early = x86_64_EarlyInitialize,
-    .initialize = x86_64_Initialize,
+  .signature = 0x686372416c774f49,
+  .initialize_early = x86_64_EarlyInitialize,
+  .initialize = x86_64_Initialize,
 
-    .disable_interrupts = x86_64_CLI,
-    .enable_interrupts = x86_64_STI,
+  .disable_interrupts = x86_64_CLI,
+  .enable_interrupts = x86_64_STI,
+
+  .get_logger = x86_64_GetLogger,
 };
