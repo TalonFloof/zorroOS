bd836c19ed62946f1f03b264b1e784a6f9e0a733
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-04-13 06:54:01 -0500

    nvm on the limine thing
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 build:
 	cd kernel; make x86_64-PC; cd ..
 
-hd: build
-	# git clone --branch v4.x-branch-binary --depth 1 https://github.com/limine-bootloader/limine /tmp/limine
+bios: build
 	./make_image.sh
 .PHONY: build
--- make_image.sh
+++ make_image.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-dd if=/dev/zero of=zorroOS.hd bs=512 count=2097152
-echo 'echo -e "o\ny\nn\n1\n\n+128M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk zorroOS.hd' | sh
-dev_mount=`losetup -f | egrep -o '[0-9]+'`
-losetup /dev/loop${dev_mount} zorroOS.hd -P
-mkfs.vfat /dev/loop${dev_mount}p1 -F 32
-# tools/fennecfstool /dev/loop${dev_mount}p2 0 newimage
-echo $(stat --format="%s/1024" /dev/loop${dev_mount}p2 | bc)
-losetup -d /dev/loop${dev_mount}
+nasm -f bin boot/x86_64/bios/Bootloader.S -o Bootloader.bin
+tools/fennecfstool zorroOS.hd 64 newimage 131072
+dd if=Bootloader.bin of=zorroOS.hd
+rm Bootloader.bin
