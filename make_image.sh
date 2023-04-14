b46c2497743c6d81fe4f1dd202d5e1adc9233fed
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-04-13 22:27:12 -0500

    the things that i did to do things
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- make_image.sh
+++ make_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nasm -f bin boot/x86_64/bios/Bootloader.S -o Bootloader.bin
 tools/fennecfstool zorroOS.hd 64 newimage 131072
-dd if=Bootloader.bin of=zorroOS.hd
+tools/fennecfstool zorroOS.hd 64 bootldr Bootloader.bin
 rm Bootloader.bin
