cb8dbc5021948392d20af5c9839062ee49c063e3
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-04-17 06:55:08 -0500

    IDT
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- kernel/architectures/x86_64/IDT.c
+++ kernel/architectures/x86_64/IDT.c
@@ -4,6 +4,31 @@
 static IDTEntry idt[256];
 static IDTP idtr;
 
+extern void* ISRTable;
+
+void IDT_Install(unsigned char entry, void* isr, unsigned char flags) {
+  IDTEntry* desc = &idt[entry];
+  desc->kernelCS = 0x28;
+  desc->attributes = flags;
+  desc->zero = 0;
+  desc->reserved = 0;
+  desc->isrLow = ((uintptr_t)isr) & 0xFFFF;
+  desc->isrMid = (((uintptr_t)isr) >> 16) & 0xFFFF;
+  desc->isrHigh = (((uintptr_t)isr) >> 32) & 0xFFFFFFFF;
+}
+
+void IRQHandler() {
+  
+}
+
 void IDT_Initialize() {
+  int i;
+  idtr.base = (uint64_t)&idt;
+  idtr.limit = (uint16_t) sizeof(IDTEntry) * 256 - 1;
   SetCurrentStatus("Loading IDT...");
+  for(i=0;i<256;i++) {
+    IDT_Install(i,(&ISRTable)[i],0x8E);
+  }
+  asm volatile ("lidt %0" :: "m" (idtr));
+  asm volatile ("sti");
 }
