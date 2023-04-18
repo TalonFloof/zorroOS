fd14a25efbdce264c0f32b9cc8d3d005dbe8db9c
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-04-18 06:54:43 -0500

    compositor stuff
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- kernel/src/Compositor/Window.c
+++ kernel/src/Compositor/Window.c
@@ -1,5 +1,33 @@
 #include <Compositor/Window.h>
+#include <Graphics/Framebuffer.h>
+#include <Utilities/String.h>
+
+Window rootWindow;
+uint32_t rootWinData[640*480];
+
+Window* windowHead;
+Window* windowTail;
+
+void Compositor_WindowRedraw() {
+    Window* win = windowHead;
+    while(win != (Window*)0) {
+        int i;
+        for(i=0;i < win->h;i++) {
+            memcpy(&(((uint32_t*)fbPtr)[((win->y+i)*fbWidth)+win->x]),&(win->data[i*win->w]),win->w*4);
+        }
+        win = win->next;
+    }
+}
 
 void Compositor_WindowSetup() {
-
+    rootWindow.next = 0;
+    rootWindow.prev = 0;
+    rootWindow.x = (fbWidth/2)-320;
+    rootWindow.y = (fbHeight/2)-240;
+    rootWindow.w = 640;
+    rootWindow.h = 480;
+    rootWindow.data = (uint32_t*)&rootWinData;
+    windowHead = &rootWindow;
+    windowTail = &rootWindow;
+    Compositor_WindowRedraw();
 }
--- kernel/src/Include/Compositor/Window.h
+++ kernel/src/Include/Compositor/Window.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <stdint.h>
+
+typedef struct {
+    void* prev;
+    void* next;
+    uint16_t x;
+    uint16_t y;
+    uint16_t w;
+    uint16_t h;
+    uint32_t* data;
+} Window;
+
+void Compositor_WindowSetup();
