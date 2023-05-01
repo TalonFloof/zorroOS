e7beb76295804c3b03e8c981c8e549639c6c3da8
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-05-01 06:54:14 -0500

    Thread stuff
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- kernel/src/thread.zig
+++ kernel/src/thread.zig
@@ -1,12 +1,19 @@
 const native = @import("native");
 const std = @import("std");
 const root = @import("root");
-const HardwareThread = root.hart.HardwareThread;
-const Spinlock = root.Spinlock;
+const HardwareThread = @import("hart.zig").HardwareThread;
+const Spinlock = @import("spinlock.zig").Spinlock;
+const object = @import("object.zig");
 
 pub const Thread = struct {
-    kstack: [8192]u8 = [_]u8{0} ** 8192,
+    obj: object.Object,
+    kstack: []u8,
     context: native.context.Context,
     floatContext: native.context.FloatContext,
     id: u64 = 0,
+    managerID: u64 = 0,
+    startTime: u64 = 0,
+    stopTime: u64 = 0,
+
+    pub fn new() *Thread {}
 };
