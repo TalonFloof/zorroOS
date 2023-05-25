776647eb35cd9fb5667a9475a87908c41cc7ab14
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-05-24 20:40:51 -0500

    things
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- ryu/kernel/main.zig
+++ ryu/kernel/main.zig
@@ -12,23 +12,6 @@ pub fn panic(msg: []const u8, stacktrace: ?*std.builtin.StackTrace, wat: ?usize)
 }
 
 pub export fn RyuInit() noreturn {
-    // Well It's the momment we've been waiting for
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024);
-    _ = Memory.Pool.StaticPool.Alloc(65024 - 16);
-    HAL.Console.Put("StaticPool | Buckets: {} UsedBlocks: {} TotalBlocks: {} FreeBlocks: {}\n", .{ Memory.Pool.StaticPool.buckets, Memory.Pool.StaticPool.usedBlocks, Memory.Pool.StaticPool.totalBlocks, Memory.Pool.StaticPool.totalBlocks - Memory.Pool.StaticPool.usedBlocks });
-    HAL.Console.Put("           | Anonymous: {} KiB Committed: {} KiB Active: {} bytes\n", .{ Memory.Pool.StaticPool.anonymousPages * 4, (Memory.Pool.StaticPool.buckets * 512) + (Memory.Pool.StaticPool.anonymousPages * 4), (Memory.Pool.StaticPool.usedBlocks * 16) + (Memory.Pool.StaticPool.anonymousPages * 4096) });
-    var dat = Memory.Pool.StaticPool.Alloc(32).?;
-    HAL.Console.Put("StaticPool | Buckets: {} UsedBlocks: {} TotalBlocks: {} FreeBlocks: {}\n", .{ Memory.Pool.StaticPool.buckets, Memory.Pool.StaticPool.usedBlocks, Memory.Pool.StaticPool.totalBlocks, Memory.Pool.StaticPool.totalBlocks - Memory.Pool.StaticPool.usedBlocks });
-    HAL.Console.Put("           | Anonymous: {} KiB Committed: {} KiB Active: {} bytes\n", .{ Memory.Pool.StaticPool.anonymousPages * 4, (Memory.Pool.StaticPool.buckets * 512) + (Memory.Pool.StaticPool.anonymousPages * 4), (Memory.Pool.StaticPool.usedBlocks * 16) + (Memory.Pool.StaticPool.anonymousPages * 4096) });
-    Memory.Pool.StaticPool.Free(dat);
-    HAL.Console.Put("StaticPool | Buckets: {} UsedBlocks: {} TotalBlocks: {} FreeBlocks: {}\n", .{ Memory.Pool.StaticPool.buckets, Memory.Pool.StaticPool.usedBlocks, Memory.Pool.StaticPool.totalBlocks, Memory.Pool.StaticPool.totalBlocks - Memory.Pool.StaticPool.usedBlocks });
-    HAL.Console.Put("           | Anonymous: {} KiB Committed: {} KiB Active: {} bytes\n", .{ Memory.Pool.StaticPool.anonymousPages * 4, (Memory.Pool.StaticPool.buckets * 512) + (Memory.Pool.StaticPool.anonymousPages * 4), (Memory.Pool.StaticPool.usedBlocks * 16) + (Memory.Pool.StaticPool.anonymousPages * 4096) });
     HAL.Console.Put("Boot Complete!\n", .{});
     while (true) {}
 }
