fa2bf385a0c6de9f1d581805e3d232d5f54ca42d
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-06-12 20:29:01 -0500

    no idea if it works, but filesystem :3
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- lib/devlib/fs.zig
+++ lib/devlib/fs.zig
@@ -1,6 +1,6 @@
 pub const Metadata = packed struct {
     deviceID: u64 = 0,
-    ID: i64 = 0,
+    ID: i64 = 0, // if -1 then an error occured
     mode: i32 = 0,
     nlinks: i32 = 1,
     uid: u32 = 1,
@@ -17,4 +17,31 @@ pub const Metadata = packed struct {
     blocks: i64 = 0,
 };
 
-pub const Mountpoint = extern struct {};
+pub const FCB = extern struct {
+    file: ?*void = null,
+    mount: ?*Mountpoint = null,
+    path: ?[*c]u8 = null,
+    offset: i64,
+    mode: usize,
+    isDir: bool,
+};
+
+pub const Mountpoint = extern struct {
+    path: [*c]const u8,
+    device: ?[*c]const u8 = null,
+    data: ?*void = null,
+
+    mount: *const fn (*Mountpoint) callconv(.C) void,
+    umount: *const fn (*Mountpoint) callconv(.C) void,
+    open: *const fn (*Mountpoint, [*c]const u8, mode: usize) callconv(.C) ?*FCB,
+    close: *const fn (*FCB) callconv(.C) void,
+    read: *const fn (*FCB, *void, i64) callconv(.C) i64,
+    write: *const fn (*FCB, *void, i64) callconv(.C) i64,
+    mkdir: *const fn (*Mountpoint, [*c]const u8) callconv(.C) bool,
+    unlink: *const fn (*Mountpoint, [*c]const u8) callconv(.C) bool,
+    stat: *const fn (*Mountpoint, [*c]const u8) callconv(.C) Metadata,
+    chown: *const fn (*Mountpoint, [*c]const u8, u32, u32) callconv(.C) bool,
+    chmod: *const fn (*Mountpoint, [*c]const u8, usize) callconv(.C) void,
+    ioctl: *const fn (*FCB, usize, *void, *c_int) callconv(.C) bool,
+    map: *const fn (*FCB, *allowzero void, usize) callconv(.C) bool,
+};
