fd73fadb246e3b555a6eac57b8da572903fb53f4
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-06-15 14:15:23 -0500

    honestly, i think i'm getting bored
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- ryu/executive/OSCalls.zig
+++ ryu/executive/OSCalls.zig
@@ -2,13 +2,42 @@ const HAL = @import("root").HAL;
 const FS = @import("root").FS;
 const Executive = @import("root").Executive;
 
-// 0x
-
 const CallCategory = enum(u16) {
     Null = 0,
     Filesystem = 1,
     Process = 2,
     Memory = 3,
+};
+
+const FilesystemFuncs = enum(u16) { // All of these are just normal UNIX calls, kinda boring tbh...
+    Open = 1,
+    Close = 2,
+    Read = 3,
+    Write = 4,
+    Seek = 5,
+};
+
+const ProcessFuncs = enum(u16) {
+    NewTeam = 1,
+    LoadExecImage = 2, // Basically exec except better since you can trigger it on either yourself or one of your child teams
+    CloneImage = 3, // Basically fork
+    NewThread = 4,
+    KillThread = 5,
+    KillTeam = 6,
+    Exit = 7,
+    GetThreadID = 8,
+    GetTeamID = 9,
+    GetParentID = 10,
+    GetRUID = 11,
+    GetRGID = 12,
+    GetEUID = 13,
+    GetEGID = 14,
+    GetGroups = 15,
+    SetRUID = 16,
+    SetRGID = 17,
+    SetEUID = 18,
+    SetEGID = 19,
+    SetGroups = 20,
 };
 
 pub export fn RyuSyscallDispatch(regs: *HAL.Arch.Context) callconv(.C) void {
@@ -17,6 +46,8 @@ pub export fn RyuSyscallDispatch(regs: *HAL.Arch.Context) callconv(.C) void {
     HAL.Console.Put("SystemCall | Cat: {x} Func: {x} ({x},{x},{x},{x},{x},{x})\n", .{ @enumToInt(cat), func, regs.GetReg(1), regs.GetReg(2), regs.GetReg(3), regs.GetReg(4), regs.GetReg(5), regs.GetReg(6) });
     switch (cat) {
         .Filesystem => {},
+        .Process => {},
+        .Memory => {},
         else => {
             regs.SetReg(0, @bitCast(u64, @intCast(i64, -4096)));
         },
