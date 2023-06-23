ade1d8e8848966670c36473bf5e851b5daf53400
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-06-23 17:09:40 -0500

    vfs PAIN
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- lib/devlib/fs.zig
+++ lib/devlib/fs.zig
@@ -22,24 +22,21 @@ pub const Inode = extern struct {
     stat: Metadata = Metadata{ .ID = 0 },
     private: *allowzero void = @intToPtr(*allowzero void, 0),
     parent: ?*Inode = null,
-    children: ?*DirEntry = null,
+    children: ?*Inode = null,
+    prevSibling: ?*Inode = null,
+    nextSibling: ?*Inode = null,
+    hasReadEntries: bool = false,
     mountOwner: ?*Inode = null,
     mountPoint: ?*Inode = null,
 
     open: ?*const fn (*Inode, usize) callconv(.C) isize = null,
     close: ?*const fn (*Inode) callconv(.C) void = null,
     read: ?*const fn (*Inode, isize, *void, isize) callconv(.C) isize = null,
-    readdir: ?*const fn (*Inode, isize) callconv(.C) ?*Inode = null,
-    finddir: ?*const fn (*Inode, [*c]const u8) callconv(.C) ?*Inode = null,
+    readdir: ?*const fn (*Inode, bool) callconv(.C) isize = null, // is called when we need to refresh the list of children
     write: ?*const fn (*Inode, isize, *void, isize) callconv(.C) isize = null,
     unlink: ?*const fn (*Inode) callconv(.C) isize = null,
     ioctl: ?*const fn (*Inode, usize, *void) callconv(.C) isize = null,
     create: ?*const fn (*Inode, [*c]const u8, usize) callconv(.C) isize = null,
     map: ?*const fn (*Inode, isize, *allowzero void, usize) callconv(.C) isize = null,
     unmap: ?*const fn (*Inode, *allowzero void, usize) callconv(.C) isize = null,
-};
-
-pub const DirEntry = extern struct {
-    inode: *Inode,
-    name: []u8,
 };
--- ryu/fs/VFS.zig
+++ ryu/fs/VFS.zig
@@ -8,12 +8,47 @@ const std = @import("std");
 const DevFS = @import("DevFS.zig");
 
 var rootInode: ?*Inode = null;
+var nextID: i64 = 2;
 var fileLock: Spinlock = .unaquired;
 
+pub fn AddInodeToParent(i: *Inode) void {
+    i.prevSibling = null;
+    if (i.parent.?.children) |head| {
+        head.prevSibling = i;
+        i.nextSibling = head;
+    } else {
+        i.nextSibling = null;
+    }
+    i.parent.?.children = i;
+}
+
 pub fn NewDirInode(name: []const u8) *Inode {
+    fileLock.acquire();
     var inode: *Inode = @ptrCast(*Inode, @alignCast(@alignOf(*Inode), Memory.Pool.PagedPool.Alloc(@sizeOf(Inode)).?.ptr));
     @memset(@intToPtr([*]u8, @ptrToInt(&inode.name))[0..256], 0);
     @memcpy(@intToPtr([*]u8, @ptrToInt(&inode.name)), name);
+    inode.parent = rootInode;
+    inode.children = null;
+    inode.stat.ID = 1;
+    inode.stat.uid = 1;
+    inode.stat.gid = 1;
+    inode.stat.nlinks = 1;
+    inode.stat.mode = 0o0040755;
+    AddInodeToParent(inode);
+    fileLock.release();
+    return inode;
 }
 
-pub fn Init() void {}
+pub fn Init() void {
+    rootInode = @ptrCast(*Inode, @alignCast(@alignOf(*Inode), Memory.Pool.PagedPool.Alloc(@sizeOf(Inode)).?.ptr));
+    rootInode.?.stat.ID = 1;
+    rootInode.?.stat.uid = 1;
+    rootInode.?.stat.gid = 1;
+    rootInode.?.stat.nlinks = 1;
+    rootInode.?.stat.mode = 0o0040755;
+    rootInode.?.children = null;
+    rootInode.?.nextSibling = null;
+    rootInode.?.prevSibling = null;
+    var devInode = NewDirInode("dev");
+    _ = devInode;
+}
